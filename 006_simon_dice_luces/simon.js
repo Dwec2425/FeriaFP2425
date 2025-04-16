041b7e4ab9e9b2b39b1faa34244992cf533ab3f9
Profe <profeluis0307@gmail.com>
2025-04-16 22:09:52 +0200

    simon v2

--- 006_simon_dice_luces/simon.js
+++ 006_simon_dice_luces/simon.js
@@ -1,40 +1,71 @@
-const AMARILLO = 0;
-const AZUL = 1;
-const VERDE = 2;
-const ROJO = 3;
-
-
 const DERROTA = 0;
 const VICTORIA = 1;
 const MAX_LEVEL = 20;
 const INITIAL_LEVEL = 1;
 
+//sonidos
 const sonido_amarillo_simon = new Audio("./audio/amarillo_simon.mp3"); 
 const sonido_azul_simon = new Audio("./audio/azul_simon.mp3"); 
 const sonido_rojo_simon = new Audio("./audio/rojo_simon.mp3"); 
 const sonido_verde_simon = new Audio("./audio/verde_simon.mp3"); 
-
-
+const sonido_error_simon = new Audio("./audio/error_simon.mp3"); 
 
 const tecla_amarilla = document.querySelector(".amarilla");
 const tecla_roja = document.querySelector(".roja");
 const tecla_verde = document.querySelector(".verde");
 const tecla_azul = document.querySelector(".azul");
 
+const nivel_div = document.querySelector(".nivel");
+const iniciar_btn = document.getElementById("iniciar_btn");
 
-
+//listeners
 tecla_amarilla.addEventListener("click", manejaClick)
 tecla_roja.addEventListener("click", manejaClick)
 tecla_verde.addEventListener("click", manejaClick)
 tecla_azul.addEventListener("click", manejaClick)
 
-let clicks = 0;
-let turno_player = false;
-let numAciertos=0;
+iniciar_btn.addEventListener("click", iniciarSecuencia);
+
+//variables del programa
 let nivel = INITIAL_LEVEL;
+let turno_player = false;
 
-const nivel_div = document.querySelector(".nivel");
+let clicks = 0; //clicks del usuario, para saber en que paso de la secuencia esta
+let numAciertos=0; //num de aciertos de la secuencia, si coinicide con el num de nivel habrá superado este
+let stepSecuencia = 0;  //digamos paso por donde esta la secuencia de la
 
+let idIntervalo = null;  //para controlar el intervalo de la secuencia de la cpu
+
+const secuencia = []; //array donde meteremos la secuencia total de MAX_LEVEL que iremos mostrando cada vez +1
+const teclasIDs = ["tecla_amarilla", "tecla_roja", "tecla_verde", "tecla_azul"];
+
+
+
+
+crearSecuencia();
+
+
+
+function iniciarSecuencia(){
+console.log("inicia secuencia cpu")
+turno_player=false;
+clicks=0; //como es el turno de la cpu, los clicks del player se ponen a cero
+iniciar_btn.disabled=true;
+    if (idIntervalo == null) {
+        idIntervalo = setInterval(() => {
+            if (stepSecuencia < nivel) {
+                accionaTecla(secuencia[stepSecuencia]);
+            }
+            stepSecuencia++;
+            if (stepSecuencia == nivel) {
+                stepSecuencia=0; //coloco el step al ppo de la secuencia
+                pararSecuencia();}
+        }, 500);
+    }
+}
+
+
+//manejar el click del usuario
 function manejaClick(e) {
 
     if(turno_player){
@@ -43,7 +74,8 @@ function manejaClick(e) {
                 console.log("acierto");
                 numAciertos++;
             }else{
-                iluminaTecla(e.target.id);
+                accionaTecla(e.target.id);
+                suena("sadf");
                 findelJuego(DERROTA);
                 console.log("fallaste");
                 return;
@@ -59,18 +91,20 @@ function manejaClick(e) {
                 }, 500);
                 
                 nivel++;
+                nivel_div.classList.add("destacar")
                 setTimeout(() => {
                     iniciarSecuencia();
+                    nivel_div.classList.remove("destacar")
                 }, 1000);
                 nivel_div.innerHTML=`Nivel #${nivel}`;
             }
               
-            iluminaTecla(e.target.id);
+            accionaTecla(e.target.id);
             
         }
         
     }else{
-        //turno cpu
+      
         
     }
     
@@ -79,64 +113,39 @@ function manejaClick(e) {
 }
 
 function setInicio(){
-    numAciertos=0;
     clicks=0;
-    nivel=1;
+    numAciertos=0;
+    nivel=INITIAL_LEVEL;
     nivel_div.innerHTML=`Nivel #${nivel}`;
+    stepSecuencia = 0;
+    idIntervalo = null; 
+    crearSecuencia();
+    setTimeout(() => {
+        iniciarSecuencia();    
+    }, 2000);
+    
 }
-function iluminaTecla(teclaID) {
+
+
+function crearSecuencia(){
+    let rand = 0;
+    
+    while(secuencia.length>0){
+        secuencia.pop(); //vacío el array
+    }
+    
+    //creo secuencia
+    for (let i = 0; i < MAX_LEVEL; i++) {
+        rand = Math.floor(Math.random() * 4);
+        secuencia.push(teclasIDs[rand]); //meto uno de los ids de las teclas de manera aleatoria
+    }
+}
+
+function accionaTecla(teclaID) {
     suena(teclaID)
     document.getElementById(`${teclaID}`).classList.add("luminosa")
     setTimeout(() => { document.getElementById(`${teclaID}`).classList.remove("luminosa") }, 300)
 }
-
-
-
-const secuencia = []
-const teclasIDs = ["tecla_amarilla", "tecla_roja", "tecla_verde", "tecla_azul"]
-
-
-
-
-let stepSecuencia = 0;
-let rand = 0;
-
-let idIntervalo = null;
-//creo secuencia
-for (let i = 0; i < MAX_LEVEL; i++) {
-    rand = Math.floor(Math.random() * 4);
-    secuencia.push(teclasIDs[rand]);
-}
-
-const iniciar_btn = document.getElementById("iniciar_btn");
-
-iniciar_btn.addEventListener("click", iniciarSecuencia);
-
-function iniciarSecuencia(){
-console.log("inicia secuencia cpu")
-    debuug();
-turno_player=false;
-clicks=0; //como es el turno de la cpu, los clicks del player se ponen a cero
-iniciar_btn.disabled=true;
-    if (idIntervalo == null) {
-        idIntervalo = setInterval(() => {
-            if (stepSecuencia < nivel) {
-                iluminaTecla(secuencia[stepSecuencia]);
-                console.log(secuencia[stepSecuencia])
-            }
-            stepSecuencia++;
-            if (stepSecuencia == nivel) {
-                stepSecuencia=0; //coloco el step al ppo de la secuencia
-                pararSecuencia();}
-        }, 500);
-    }
-}
-
-
-
-
-
-
 
 function pararSecuencia() {
     clearInterval(idIntervalo);
@@ -145,17 +154,17 @@ function pararSecuencia() {
     setTimeout(()=>{iniciar_btn.disabled=false},500);
 }
 
-//iniciar_btn.click();
-
 
 function findelJuego(resultado){
     console.log("fin del juego")
     //clicks=nivel; //asi no puedes seguir haciendo clicks
     if(resultado==DERROTA){
         setInicio();
+        nivel_div.classList.add("destacar")
         document.body.classList.toggle("fail");
         setTimeout(() => {
             document.body.classList.toggle("fail");
+            nivel_div.classList.remove("destacar")
         }, 1500);
         
     }else{alert("ganaste");}
@@ -166,13 +175,6 @@ function muestraFormulario(){
     console.warn("hay que implementar el formulario para scores")
 }
 
-function debuug(){
-    console.log("nivel",nivel)
-    console.log("clicks",clicks)
-    console.log("num aciertos",numAciertos)
-    
-
-}
 
 
 function suena(color){
@@ -196,6 +198,10 @@ function suena(color){
             sonido_verde_simon.currentTime = 0; //al ppo
             sonido_verde_simon.play();
             break;
+        }
+        default: {
+            sonido_error_simon.currentTime = 0; //al ppo
+            sonido_error_simon.play();
         }
     }
     
